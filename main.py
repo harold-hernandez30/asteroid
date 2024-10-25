7ba2d2e3c37fda1fa40050772933d1d792141c48
Harold Hernandez <harold.hernandez@visma.com>
2024-10-25 10:53:10 +0200

    Working sprite.groups - separate drawable and updatable

--- main.py
+++ main.py
@@ -9,21 +9,32 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
+
+    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     black = 0, 0, 0
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    hero = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                  return
+
         screen.fill(black)
-        hero.draw(screen)
-        pygame.display.flip()
-        hero.update(dt)
         dt = clock.tick(60) / 1000
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+        for sprite in updatable:
+            sprite.update(dt)
+            
+        pygame.display.flip()
+             
 
 
 if __name__ == "__main__":
